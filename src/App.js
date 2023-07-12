3d4d6fddb4376a35026bb56fa4a716a5b2df78af
nojiseon16 <nojiseon16@nojiseon16ui-MacBookPro.local>
2023-07-12 14:31:26 +0900

    푸터 완료

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Header from "./Header";
 import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>개인정보 처리방침 | 이용 약관</div>
+      <div>All rights reserved @ Codestates</div>
+    </footer>
+  );
+};
+export default Footer;
