f0560f3eec34987a28edb39df3cd5fdbd22d407d
nojiseon16 <nojiseon16@nojiseon16ui-MacBookPro.local>
2023-07-14 14:33:10 +0900

    모달 이미지 팝업

--- src/Main.js
+++ src/Main.js
@@ -10,13 +10,26 @@ const Main = () => {
   const [productItem, setProductItem] = useState([]);
 
   const [isPop, setIsPop] = useState(false);
+  const [tempSrc, setTempSrc] = useState("");
+  const [tempTit, setTempTit] = useState("");
+  const [elId, elSetId] = useState("");
+  // const [isChk, setIsChk] = useState(false);
+
+  const getImg = (id, title, image_url, brand_image_url) => {
+    brand_image_url === null ? setTempSrc(image_url) : setTempSrc(brand_image_url);
+    setIsPop(true);
+    // setIsChk(!isChk);
+    setTempTit(title);
+    elSetId(id);
+  };
+
   const popHandler = () => {
     setIsPop(!isPop);
   };
 
-  //   useEffect(() => {
-  //     localStorage.setItem("productItem", JSON.stringify(productItem));
-  //   }, [productItem]);
+  // const chkHandler = () => {
+  //   setIsChk(!isChk);
+  // };
 
   useEffect(() => {
     fetch(`http://cozshopping.codestates-seb.link/api/v1/products?count=4`)
@@ -24,27 +37,31 @@ const Main = () => {
       .then(data => setProductItem(data));
   }, []);
 
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch("http://cozshopping.codestates-seb.link/api/v1/products?count=4");
-  //       const data = await response.json();
-  //       localStorage.setItem("fetchedData", JSON.stringify(data));
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch("http://cozshopping.codestates-seb.link/api/v1");
+  //     const data = await response.json();
+  //     localStorage.setItem("fetchedData", JSON.stringify(data));
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
 
   return (
     <div className="main_page">
       <h2 className="mainTit">상품 리스트</h2>
       <ul className="list">
         {productItem.map(item => (
-          <li key={item.id} onClick={() => popHandler(item)}>
-            <ImgModal item={item} />
+          <li key={item.id} onClick={() => getImg(item.id, item.title, item.image_url, item.brand_image_url)}>
             <div className="imgConbox">
               <img src={item.brand_image_url === null ? item.image_url : item.brand_image_url} />
-              <input type="checkbox" id={item.id} className="hide inp_star" />
-              <label htmlFor={item.id} className="label_star"></label>
+              <input
+                type="checkbox"
+                id={item.id}
+                className="hide inp_star"
+                onClick={event => event.stopPropagation()}
+              />
+              <label htmlFor={item.id} className="label_star" onClick={event => event.stopPropagation()}></label>
             </div>
 
             {item.type === "Product" ? (
@@ -59,6 +76,8 @@ const Main = () => {
           </li>
         ))}
       </ul>
+
+      <ImgModal isPop={isPop} tempSrc={tempSrc} tempTit={tempTit} elId={elId} popHandler={popHandler} />
 
       <h2 className="mainTit">북마크 리스트</h2>
     </div>
